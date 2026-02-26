e9f93621a7e5d38be0c6860002ed9fc1939bf519
Heer <heerpatel0501@gmail.com>
2026-02-26 12:12:40 +0530

    upload changes

--- app.py
+++ app.py
@@ -1,31 +1,22 @@
-from flask import Flask, render_template, request
-from datetime import datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template("index.html")
 
-@app.route("/dob", methods=["GET", "POST"])
-def dob():
-    age = None
+@app.route('/skills')
+def skills():
+    return render_template("skills.html")
 
-    if request.method == "POST":
-        dob_input = request.form["dob"]
-        birth_date = datetime.strptime(dob_input, "%Y-%m-%d")
-        today = datetime.today()
-        age = today.year - birth_date.year
+@app.route('/finance')
+def finance():
+    return render_template("finance.html")
 
-    return render_template("dob.html", age=age)
-
-@app.route("/career")
-def career():
-    return render_template("career.html")
-
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
+@app.route('/health')
+def health():
+    return render_template("health.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
