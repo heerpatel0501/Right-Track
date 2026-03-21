5320338413d888c96b68d8e505c86ebca5f18bce
Riya Patidar <heerpatel0501@gmail.com>
2026-03-21 10:33:13 +0530

    Fixed css files

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,31 @@
+const skillsData = {
+    coding: {
+        title: "Coding 💻",
+        do: "Start with HTML, CSS basics",
+        why: "Coding builds logic and problem solving",
+        action: "Code 30 mins daily"
+    },
+    fitness: {
+        title: "Fitness 🏃",
+        do: "Do basic exercise daily",
+        why: "Keeps body and mind healthy",
+        action: "15 min workout today"
+    },
+    reading: {
+        title: "Reading 📖",
+        do: "Read books or articles",
+        why: "Improves knowledge and focus",
+        action: "Read 10 pages today"
+    }
+};
+function showSkill(skill) {
+    document.getElementById("skill-title").innerText = skillsData[skill].title;
+    document.getElementById("skill-do").innerText = skillsData[skill].do;
+    document.getElementById("skill-why").innerText = skillsData[skill].why;
+    document.getElementById("skill-action").innerText = skillsData[skill].action;
+
+    document.getElementById("skill-info").classList.remove("hidden");
+}
 function selectSkill(card){
     document.querySelectorAll(".card").forEach(c => c.classList.remove("active"));
 event.currentTarget.classList.add("active");
